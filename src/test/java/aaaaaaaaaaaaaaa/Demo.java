4ab122d17ee102a7537419fd35148a3219a6bf1e
QSP <QSP@QSP>
2021-05-29 22:27:42 +0530

    Om

---
+++ src/test/java/aaaaaaaaaaaaaaa/Demo.java
@@ -0,0 +1,12 @@
+package aaaaaaaaaaaaaaa;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void demo() {
+		Reporter.log("sai ram",true);
+	}
+
+}
